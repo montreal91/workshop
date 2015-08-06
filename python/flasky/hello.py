e39cf22e43e3a3340b01891190618a6a39dd5680
Alexander <nefedov.alexander91@yandex.ru>
2015-08-06 08:43:37 +0500

    Initial commit on flasky tutorial blog
    New files:
    	.gitignore
    	python/flasky/hello.py

---
+++ python/flasky/hello.py
@@ -0,0 +1,19 @@
+
+#! /usr/bin/python
+
+from flask              import Flask
+from flask.ext.script   import Manager
+
+app     = Flask( __name__ )
+manager = Manager( app )
+
+@app.route( "/" )
+def Index():
+    return "<h1>Hello World!</h1>"
+
+@app.route( "/user/<name>/" )
+def User( name ):
+    return "<h1>Hello, %s</h1>" % name
+
+if __name__ == '__main__':
+    manager.run()
