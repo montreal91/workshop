5fe0dc349c0dfaf73ce3d0ee500addd527aedeb1
Alexander <nefedov.alexander91@yandex.ru>
2015-08-06 11:02:03 +0500

    Made use of flask-wtforms
    Modified:
    	python/flasky/hello.py
    	python/flasky/templates/base.html
    	python/flasky/templates/index.html

--- python/flasky/hello.py
+++ python/flasky/hello.py
@@ -3,16 +3,30 @@
 
 from datetime               import datetime
 
-from flask                  import Flask, render_template
+from flask                  import Flask
+from flask                  import redirect, session, url_for
+from flask                  import flash, render_template
 from flask.ext.bootstrap    import Bootstrap
 from flask.ext.script       import Manager
 from flask.ext.moment       import Moment
+from flask.ext.wtf          import Form
+
+from wtforms                import StringField, SubmitField
+from wtforms.validators     import Required
+
 
 app         = Flask( __name__ )
+app.config[ "SECRET_KEY" ] = "vivid vervet"
+
+manager     = Manager( app )
 bootstrap   = Bootstrap( app )
 moment      = Moment( app )
 
-manager     = Manager( app )
+
+
+class NameForm( Form ):
+    name    = StringField( "What is your name?", validators=[ Required() ] )
+    submit  = SubmitField( "Submit" )
 
 
 @app.errorhandler( 404 )
@@ -25,13 +39,17 @@ def InternalServerError( e ):
     return render_template( "500.html" ), 500
 
 
-@app.route( "/" )
+@app.route( "/", methods=[ "GET", "POST" ] )
 def Index():
-    return render_template( "index.html", current_time=datetime.utcnow() )
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get( "name" )
+        if old_name is not None and old_name != form.name.data:
+            flash( "Looks like you have changed your name!" )
+        session[ "name" ] = form.name.data
+        return redirect( url_for( "Index" ) )
+    return render_template( "index.html", form=form, name=session.get( "name" ) )
 
-@app.route( "/user/<name>/" )
-def User( name ):
-    return render_template( "user.html", name=name )
 
 if __name__ == '__main__':
     manager.run()
