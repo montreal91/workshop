264e6a5aee74d80474521b46a7191ff5b4fc10b1
Alexander <nefedov.alexander91@yandex.ru>
2015-09-01 01:30:12 +0500

    First steps with pure C
    New files:
    	C/learn_c_hard_way/ex1/ex1
    	C/learn_c_hard_way/ex1/ex1.c

---
+++ C/learn_c_hard_way/ex1/ex1.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main( int argc, char* argv[] ) {
+    puts( "Hello, World!" );
+    puts( "And fuck everything!" );
+    puts( "I've got tired from this shit." );
+    
+    return 0;
+}
