4bf91ea471b5c41557bbc7bc1ffa01d9cf06e205
Alexander <nefedov.alexander91@yandex.ru>
2015-10-27 20:30:20 +0500

    [Tutorials] LoadingState class for SFML tutorial is implemented 	tutorials/airstrike-sfml/Sources/LoadingState.cpp 	tutorials/airstrike-sfml/Sources/LoadingState.hpp

---
+++ tutorials/airstrike-sfml/Sources/LoadingState.hpp
@@ -0,0 +1,33 @@
+
+#ifndef __LOADING_STATE_HPP__
+#define __LOADING_STATE_HPP__
+
+
+#include "State.hpp"
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+#include "ParallelTask.hpp"
+
+
+class LoadingState : public State {
+public:
+    LoadingState( StateStack& stack, Context context );
+
+    virtual void    draw();
+    virtual bool    update( sf::Time dt );
+    virtual bool    handleEvent( const sf::Event& event );
+
+    void            setCompletion( float percent );
+
+private:
+    sf::Text            mLoadingText;
+    sf::RectangleShape  mProgressBarBackground;
+    sf::RectangleShape  mProgressBar;
+
+    ParallelTask        mLoadingTask;
+
+};
+
+#endif
