135c68606c3b81ebf77746d85416c3f1df7f4de2
Alexander <nefedov.alexander91@yandex.ru>
2015-10-28 11:31:58 +0500

    [Tutorials] StateStack class for SFML tutorial
    New files:
    	tutorials/airstrike-sfml/Sources/StateIdentifiers.hpp
    	tutorials/airstrike-sfml/Sources/StateStack.cpp
    	tutorials/airstrike-sfml/Sources/StateStack.hpp

---
+++ tutorials/airstrike-sfml/Sources/StateIdentifiers.hpp
@@ -0,0 +1,17 @@
+
+#ifndef __STATE_IDENTIFIERS_HPP__
+#define __STATE_IDENTIFIERS_HPP__
+
+
+namespace States {
+    enum ID {
+        None,
+        Title,
+        Menu,
+        Game,
+        Loading,
+        Pause,
+    };
+}
+
+#endif
---
+++ tutorials/airstrike-sfml/Sources/StateStack.hpp
@@ -0,0 +1,75 @@
+
+#ifndef __STATE_STACK_HPP__
+#define __STATE_STACK_HPP__
+
+
+#include "State.hpp"
+#include "StateIdentifiers.hpp"
+#include "ResourceIdentifiers.hpp"
+
+#include <SFML/System/NonCopyable.hpp>
+#include <SFML/System/Time.hpp>
+
+#include <vector>
+#include <utility>
+#include <functional>
+#include <map>
+
+
+namespace sf {
+    class Event;
+    class RenderWindow;
+}
+
+
+class StateStack : private sf::NonCopyable {
+public:
+    enum Action {
+        Push,
+        Pop,
+        Clear,
+    };
+
+    explicit StateStack( State::Context context );
+
+    template <typename T>
+    void registerState( States::ID stateID );
+
+    void update( sf::Time dt );
+    void draw();
+    void handleEvent( const sf::Event& event );
+
+    void pushState( States::ID stateID );
+    void popState();
+    void clearStates();
+
+    bool isEmpty() const;
+
+private:
+    State::Ptr  createState( States::ID stateID );
+    void        applyPendingChanges();
+
+    struct PendingChange {
+        explicit PendingChange( Action action, States::ID stateID = States::None );
+
+        Action      action;
+        States::ID  stateID;
+    };
+
+    std::vector<State::Ptr>                             mStack;
+    std::vector<PendingChange>                          mPendingList;
+
+    State::Context                                      mContext;
+    std::map<States::ID, std::function<State::Ptr()>>   mFactories;
+
+};
+
+template <typename T>
+void
+StateStack::registerState( States::ID stateID ) {
+    mFactories[ stateID ] = [this] () {
+        return State::Ptr( new T( *this, mContext ) );
+    };
+}
+
+#endif
