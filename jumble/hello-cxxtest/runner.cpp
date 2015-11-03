53b47562e387004f1796a1e74cc53bb00c139d9c
Alexander <nefedov.alexander91@yandex.ru>
2015-11-03 17:12:41 +0500

    [Jumble] Hello, CxxTest
    New files:
    	jumble/hello-cxxtest/MyTestSuite1.h
    	jumble/hello-cxxtest/runner.cpp

---
+++ jumble/hello-cxxtest/runner.cpp
@@ -0,0 +1,37 @@
+/* Generated file, do not edit */
+
+#ifndef CXXTEST_RUNNING
+#define CXXTEST_RUNNING
+#endif
+
+#define _CXXTEST_HAVE_STD
+#include <cxxtest/TestListener.h>
+#include <cxxtest/TestTracker.h>
+#include <cxxtest/TestRunner.h>
+#include <cxxtest/RealDescriptions.h>
+#include <cxxtest/TestMain.h>
+#include <cxxtest/ErrorPrinter.h>
+
+int main( int argc, char *argv[] ) {
+ int status;
+    CxxTest::ErrorPrinter tmp;
+    CxxTest::RealWorldDescription::_worldName = "cxxtest";
+    status = CxxTest::Main< CxxTest::ErrorPrinter >( tmp, argc, argv );
+    return status;
+}
+bool suite_MyTestSuite_init = false;
+#include "/home/montreal/Programming/workshop/jumble/hello-cxxtest/MyTestSuite1.h"
+
+static MyTestSuite suite_MyTestSuite;
+
+static CxxTest::List Tests_MyTestSuite = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_MyTestSuite( "MyTestSuite1.h", 4, "MyTestSuite", suite_MyTestSuite, Tests_MyTestSuite );
+
+static class TestDescription_suite_MyTestSuite_testAddition : public CxxTest::RealTestDescription {
+public:
+ TestDescription_suite_MyTestSuite_testAddition() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 6, "testAddition" ) {}
+ void runTest() { suite_MyTestSuite.testAddition(); }
+} testDescription_suite_MyTestSuite_testAddition;
+
+#include <cxxtest/Root.cpp>
+const char* CxxTest::RealWorldDescription::_worldName = "cxxtest";
