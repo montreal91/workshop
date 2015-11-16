405e36ed4fb475a6c5bfbf9aa0fc614495cef264
Alexander <nefedov.alexander91@yandex.ru>
2015-11-16 15:10:36 +0500

    [Tutorials] New categories of objects in SFML tutorial
    Mosified:
    	tutorials/airstrike-sfml/Sources/Category.hpp

--- tutorials/airstrike-sfml/Sources/Category.hpp
+++ tutorials/airstrike-sfml/Sources/Category.hpp
@@ -3,13 +3,19 @@
 #define __CATEGORY_HPP__
 
 namespace Category {
-    enum Type {
-        None            = 0,
-        Scene           = 1 << 0,
-        PlayerAircraft  = 1 << 1,
-        AlliedAircraft  = 1 << 2,
-        EnemyAircraft   = 1 << 3,
-    };
-}
+enum Type {
+    None                = 0,
+    SceneAirLayer       = 1 << 0,
+    PlayerAircraft      = 1 << 1,
+    AlliedAircraft      = 1 << 2,
+    EnemyAircraft       = 1 << 3,
+    Pickup              = 1 << 4,
+    AlliedProjectile    = 1 << 5,
+    EnemyProjectile     = 1 << 6,
 
-#endif
+    Aircraft            = PlayerAircraft | AlliedAircraft | EnemyAircraft,
+    Projectile          = AlliedProjectile | EnemyProjectile,
+};
+} // namespace Category
+
+#endif // __CATEGORY_HPP__
