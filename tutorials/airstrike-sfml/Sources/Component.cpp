ca2e0e58106f51f48780e4d938aaf0c27cc7c6b5
Alexander <nefedov.alexander91@yandex.ru>
2015-11-17 13:28:34 +0500

    [Tutorials] New component class
    SFML-Tutorials
    
    New files:
    	tutorials/airstrike-sfml/Sources/Component.cpp
    	tutorials/airstrike-sfml/Sources/Component.hpp

---
+++ tutorials/airstrike-sfml/Sources/Component.cpp
@@ -0,0 +1,42 @@
+
+#include "Component.hpp"
+
+namespace GUI {
+
+Component::Component() :
+mIsSelected( false ),
+mIsActive( false ) {}
+
+Component::~Component() {}
+
+bool
+Component::isSelected() const {
+    return mIsSelected;
+}
+
+void
+Component::select() {
+    mIsSelected = true;
+}
+
+void
+Component::deselect() {
+    mIsSelected = false;
+}
+
+bool
+Component::isActive() const {
+    return mIsActive;
+}
+
+void
+Component::activate() {
+    mIsActive = true;
+}
+
+void
+Component::deactivate() {
+    mIsActive = false;
+}
+
+} // namespace GUI
---
+++ tutorials/airstrike-sfml/Sources/Component.hpp
@@ -0,0 +1,43 @@
+
+#ifndef __COMPONENT_HPP__
+#define __COMPONENT_HPP__
+
+
+#include <SFML/System/NonCopyable.hpp>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+
+#include <memory>
+
+
+namespace sf {
+class Event;
+}
+
+namespace GUI {
+
+class Component : public sf::Drawable, public sf::Transformable, private sf::NonCopyable {
+public:
+    typedef std::shared_ptr<Component> Ptr;
+
+            Component();
+    virtual ~Component();
+
+    virtual bool    isSelectable() const = 0;
+    bool            isSelected() const;
+    virtual void    select();
+    virtual void    deselect();
+
+    virtual bool    isActive() const;
+    virtual void    activate();
+    virtual void    deactivate();
+
+    virtual void    handleEvent( const sf::Event& event ) = 0;
+
+private:
+    bool mIsSelected;
+    bool mIsActive;
+};
+} // namespace GUI
+
+#endif // __COMPONENT_HPP__
