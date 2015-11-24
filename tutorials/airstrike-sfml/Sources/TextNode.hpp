b595b8ce0d3e335ea8c2b2edcad0863c1d91584a
Alexander <nefedov.alexander91@yandex.ru>
2015-11-25 02:14:52 +0500

    [Tutorials] New TextNode class
    SFML Tutorial
    It is SceneGraph node designed to render vital information about
    another scene nodes. Derives from SceneNode class.
    
    New files:
    	tutorials/airstrike-sfml/Sources/TextNode.cpp
    	tutorials/airstrike-sfml/Sources/TextNode.hpp

---
+++ tutorials/airstrike-sfml/Sources/TextNode.hpp
@@ -0,0 +1,25 @@
+
+#ifndef __TEXT_NODE_HPP__
+#define __TEXT_NODE_HPP__
+
+
+#include "ResourceHolder.hpp"
+#include "ResourceIdentifiers.hpp"
+#include "SceneNode.hpp"
+
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Font.hpp>
+
+
+class TextNode : public SceneNode {
+public:
+    explicit    TextNode( const FontHolder& fonts, const std::string& text );
+    void        setString( const std::string& text );
+
+private:
+    virtual void    drawCurrent( sf::RenderTarget& target, sf::RenderStates states ) const;
+
+    sf::Text        mText;
+};
+
+#endif //__TEXT_NODE_HPP__
