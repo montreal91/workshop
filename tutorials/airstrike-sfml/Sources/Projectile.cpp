126e9fa1719fe1776ecbc56f6f95f51cf8ce0609
Alexander <nefedov.alexander91@yandex.ru>
2015-11-24 17:13:07 +0500

    [Tutorials] New Projectile class
    Describes such things as bullets and missiles.
    
    SFML Tutorial
    
    New files:
    	tutorials/airstrike-sfml/Sources/Projectile.cpp
    	tutorials/airstrike-sfml/Sources/Projectile.hpp

---
+++ tutorials/airstrike-sfml/Sources/Projectile.cpp
@@ -0,0 +1,80 @@
+
+#include "Projectile.hpp"
+#include "DataTables.hpp"
+#include "Utility.hpp"
+#include "ResourceHolder.hpp"
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+
+#include <cmath>
+#include <cassert>
+
+
+namespace {
+    const std::vector<ProjectileData> Table = initalizeProjectileData();
+}
+
+Projectile::Projectile( Type type, const TextureHolder& textures ) :
+Entity( 1 ),
+mType( type ),
+mSprite( textures.get( Table[type].texture ) ),
+mTargetDirection() {
+    centerOrigin( mSprite );
+}
+
+void
+Projectile::guideTowards( sf::Vector2f position ) {
+    assert( isGuided() );
+    mTargetDirection = unitVector( position - getWorldPosition() );
+}
+
+bool
+Projectile::isGuided() const {
+    return mType == Missile;
+}
+
+void
+Projectile::updateCurrent( sf::Time dt, CommandQueue& commands ) {
+    if ( isGuided() ) {
+        const float approachRate = 200.0f;
+
+        sf::Vector2f newVelocity = unitVector( approachRate * dt.asSeconds() * mTargetDirection + getVelocity() );
+        newVelocity *= getMaxSpeed();
+        float angle = std::atan2( newVelocity.y, newVelocity.x );
+
+        setRotation( toDegree( angle ) + 90.0f );
+        setVelocity( newVelocity );
+    }
+
+    Entity::updateCurrent( dt, commands );
+}
+
+void
+Projectile::drawCurrent( sf::RenderTarget& target, sf::RenderStates states ) const {
+    target.draw( mSprite, states );
+}
+
+unsigned int
+Projectile::getCategory() const {
+    if ( mType == EnemyBullet ) {
+        return Category::EnemyProjectile;
+    } else {
+        return Category::AlliedProjectile;
+    }
+}
+
+sf::FloatRect
+Projectile::getBoundingRect() const {
+    return getWorldTransform().transformRect( mSprite.getGlobalBounds() );
+}
+
+float
+Projectile::getMaxSpeed() const {
+    return Table[mType].speed;
+}
+
+int
+Projectile::getDamage() const {
+    return Table[mType].damage;
+}
---
+++ tutorials/airstrike-sfml/Sources/Projectile.hpp
@@ -0,0 +1,40 @@
+
+#ifndef __PROJECTILE_HPP__
+#define __PROJECTILE_HPP__
+
+
+#include "Entity.hpp"
+#include "ResourceIdentifiers.hpp"
+
+#include <SFML/Graphics/Sprite.hpp>
+
+
+class Projectile : public Entity {
+public:
+    enum Type {
+        AlliedBullet,
+        EnemyBullet,
+        Missile,
+        TypeCount,
+    };
+
+    Projectile( Type type, const TextureHolder& textures );
+    void                    guideTowards( sf::Vector2f position );
+    bool                    isGuided() const;
+
+    virtual unsigned int    getCategory() const;
+    virtual sf::FloatRect   getBoundingRect() const;
+    float                   getMaxSpeed() const;
+    int                     getDamage() const;
+
+private:
+    virtual void    updateCurrent( sf::Time dt, CommandQueue& commands );
+    virtual void    drawCurrent( sf::RenderTarget& target, sf::RenderStates states ) const;
+
+    Type            mType;
+    sf::Sprite      mSprite;
+    sf::Vector2f    mTargetDirection;
+
+};
+
+#endif
