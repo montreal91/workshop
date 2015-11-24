32624dfedeb0a4b24d82f8c6e187075e46631a2d
Alexander <nefedov.alexander91@yandex.ru>
2015-11-25 02:17:01 +0500

    [Tutorials] New utility functions
    SFML Tutorial
    
    Modified:
    	tutorials/airstrike-sfml/Sources/Utility.cpp
    	tutorials/airstrike-sfml/Sources/Utility.hpp

--- tutorials/airstrike-sfml/Sources/Utility.hpp
+++ tutorials/airstrike-sfml/Sources/Utility.hpp
@@ -4,15 +4,27 @@
 
 #include <sstream>
 
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/System/Vector2.hpp>
+
 
 namespace sf {
     class Sprite;
     class Text;
-}
+} // namespace sf
+
+std::string     toString( sf::Keyboard::Key key );
+
+void            centerOrigin( sf::Sprite& sprite );
+void            centerOrigin( sf::Text& text );
+
+float           toDegree( float radian );
+float           toRadian( float degree );
+
+int             randomInt( int exclusiveMax );
+
+float           length( sf::Vector2f vector );
+sf::Vector2f    unitVector( sf::Vector2f vector );
 
 
-void centerOrigin( sf::Sprite& sprite );
-void centerOrigin( sf::Text& text );
-
-
-#endif
+#endif // __UTILITY_HPP__
