f1de04c92a199c508857a081bfdc44993095d4aa
Alexander <nefedov.alexander91@yandex.ru>
2015-12-11 05:35:17 +0500

    [Forgery] JGame class is refactored
    Most of the code from PlayTextGame is outsourced to _ProcessUserInput
    
    Modified:
    	forgery/simple-manager/game.py

--- forgery/simple-manager/game.py
+++ forgery/simple-manager/game.py
@@ -1,5 +1,13 @@
 from league import JLeague
 
+class JStruct:
+    pass
+
+
+PROCESS_INPUT_CODES = JStruct()
+PROCESS_INPUT_CODES.DEFAULT = 0
+PROCESS_INPUT_CODES.HAS_TO_EXIT = 1
+
 class JGame( object ):
     def __init__(self):
         super( JGame, self ).__init__()
@@ -10,31 +18,41 @@ class JGame( object ):
         self._league.CreateSchedule()
         self._players_club = 1
 
+    def _ShowPlayerCurrentMatch(self):
+        res = self._league.GetCurrentMatchByClubId(self._players_club)
+        if res:
+            print("Your match is")
+            print("\t", str(res))
+        else:
+            print("You have no upcoming matches.")
+
+    def _ProcessUserInput(self, user_input):
+        if user_input == "show":
+            print("Day:", self._league.current_day)
+            print(self._league.current_matches)
+            return PROCESS_INPUT_CODES.DEFAULT
+        elif user_input == "q":
+            return PROCESS_INPUT_CODES.HAS_TO_EXIT
+        elif user_input == "n":
+            self._league.PlayCurrentMatches()
+            print("Today's results")
+            for match in self._league.current_matches:
+                print("\t{0:s} {1:d}:{2:d}".format(str(match), match.score[0], match.score[1]))
+            self._league.NextDay()
+            return PROCESS_INPUT_CODES.DEFAULT
+        elif user_input == "m":
+            self._ShowPlayerCurrentMatch()
+            return PROCESS_INPUT_CODES.DEFAULT
+        else:
+            print("Please, enter a valid command")
+            return PROCESS_INPUT_CODES.DEFAULT
+
     # Text-based interface for testing purposes
     def PlayTextGame(self):
         have_to_exit = False
         while not have_to_exit:
-            user_input = input("> ")
-            if user_input == "show":
-                print("Day:", self._league.current_day)
-                print(self._league.current_matches)
-            elif user_input == "q":
+            if self._ProcessUserInput(input("> ")) == PROCESS_INPUT_CODES.HAS_TO_EXIT:
                 have_to_exit = True
-            elif user_input == "n":
-                print("Today's results")
-                for match in self._league.current_matches:
-                    res = self._league.QuickSimResult()
-                    print("\t#{0:d} {1:d}:{2:d} #{3:d}".format(match[0], res[0], res[1], match[1]))
-                self._league.NextDay()
-            elif user_input == "m":
-                res = self._league.GetCurrentMatchByClubId(self._players_club)
-                if res:
-                    print("Your match is")
-                    print("\t", res)
-                else:
-                    print("You have no upcoming matches.")
-            else:
-                print("Please, enter a valid command")
             if self._league.current_day >= self._league.days:
                 print("The season is over")
                 have_to_exit = True
