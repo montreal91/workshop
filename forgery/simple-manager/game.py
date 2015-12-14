9687592536e3650394946e2f37a01ef96bc4779e
Alexander <nefedov.alexander91@yandex.ru>
2015-12-14 13:26:32 +0500

    [Forgery] JGame class is refactored
    Code from elif branches is outsourced to separate methods.
    
    Modified:
    	forgery/simple-manager/game.py

--- forgery/simple-manager/game.py
+++ forgery/simple-manager/game.py
@@ -1,7 +1,7 @@
-from random import shuffle, seed
+from random import shuffle
 
 from league import JLeague
-from club import JClub
+from club   import JClub
 
 class JStruct:
     pass
@@ -50,56 +50,71 @@ class JGame( object ):
         else:
             print("You have no upcoming matches.")
 
+    def _ProcessShowCommand(self):
+        print("Day:", self._league.current_day)
+        if self._league.current_matches:
+            print(self._league.current_matches)
+        else:
+            print("No matches for today.")
+        return PROCESS_INPUT_CODES.DEFAULT
+
+    def _ProcessNextCommand(self):
+        if not self._IsPlayableClubReady():
+            return PROCESS_INPUT_CODES.HAVE_TO_SELECT_PLAYER
+        if self._league.current_matches:
+            self._league.PlayCurrentMatches()
+            print("Today's results")
+            for match in self._league.current_matches:
+                print("\t{0:s} {1:d}:{2:d}".format(str(match), match.score[0], match.score[1]))
+            self._league.NextDay()
+        else:
+            print("All quiet today")
+            self._league.NextDay()
+        return PROCESS_INPUT_CODES.DEFAULT
+
+    def _ProcessSelectCommand(self, tokens):
+        club = self._league.GetClubById(self._players_club)
+        if len(tokens) == 1 or "r" in tokens:
+            club.SelectRandomPlayer()
+            print("Randomly selected player is {0:2d}".format(club.selected_player))
+            return PROCESS_INPUT_CODES.DEFAULT
+        elif len(tokens) == 2:
+            pl = int(tokens[1])
+            if pl in club.players:
+                club.SelectPlayerById(pl)
+                print("You have selected player {0:2d} for the next match".format(pl))
+                return PROCESS_INPUT_CODES.DEFAULT
+            else:
+                print("Your club does not have this player!")
+                return PROCESS_INPUT_CODES.DEFAULT
+        else:
+            print("Please, enter a valid command.")
+            return PROCESS_INPUT_CODES.DEFAULT
+
+    def _ProcessMyPlayersCommand(self):
+        club = self._league.GetClubById(self._players_club)
+        for player in club.players:
+            if club.selected_player == player:
+                print("{0:2d} <".format(player))
+            else:
+                print("{0:2d}".format(player))
+        return PROCESS_INPUT_CODES.DEFAULT
+
     def _ProcessUserInput(self, user_input):
         tokens = user_input.split()
         if "show" in tokens:
-            print("Day:", self._league.current_day)
-            if self._league.current_matches:
-                print(self._league.current_matches)
-            else:
-                print("No matches for today.")
-            return PROCESS_INPUT_CODES.DEFAULT
+            return self._ProcessShowCommand()
         elif "q" in tokens:
             return PROCESS_INPUT_CODES.HAS_TO_EXIT
         elif "n" in tokens:
-            if not self._IsPlayableClubReady():
-                return PROCESS_INPUT_CODES.HAVE_TO_SELECT_PLAYER
-            if self._league.current_matches:
-                self._league.PlayCurrentMatches()
-                print("Today's results")
-                for match in self._league.current_matches:
-                    print("\t{0:s} {1:d}:{2:d}".format(str(match), match.score[0], match.score[1]))
-                self._league.NextDay()
-            else:
-                print("All quiet today")
-                self._league.NextDay()
-            return PROCESS_INPUT_CODES.DEFAULT
+            return self._ProcessNextCommand()
         elif "m" in tokens:
             self._ShowPlayerCurrentMatch()
             return PROCESS_INPUT_CODES.DEFAULT
-
         elif "s" in tokens:
-            club = self._league.GetClubById(self._players_club)
-            if len(tokens) == 1 or "r" in tokens:
-                club.SelectRandomPlayer()
-                print("Randomly selected player is {0:2d}".format(club.selected_player))
-            elif len(tokens) == 2:
-                pl = int(tokens[1])
-                if pl in club.players:
-                    club.SelectPlayerById(pl)
-                    print("You have selected player {0:2d} for the next match".format(pl))
-                    return PROCESS_INPUT_CODES.DEFAULT
-                else:
-                    print("Your club does not have this player!")
-                    return PROCESS_INPUT_CODES.DEFAULT
-
+            return self._ProcessSelectCommand(tokens)
         elif "mp" in tokens:
-            club = self._league.GetClubById(self._players_club)
-            for player in club.players:
-                if club.selected_player == player:
-                    print("{0:2d}< ".format(player))
-                else:
-                    print("{0:2d}".format(player))
+            return self._ProcessMyPlayersCommand()
         else:
             print("Please, enter a valid command")
             return PROCESS_INPUT_CODES.DEFAULT
@@ -123,11 +138,10 @@ class JGame( object ):
                 print("You have to select player!")
                 continue
             if self._league.current_day >= self._league.days:
-                print("The season is over")
+                print("The season is over.")
                 have_to_exit = True
 
 
 if __name__ == "__main__":
-    # seed(39)
     game = JGame()
     game.PlayTextGame()
