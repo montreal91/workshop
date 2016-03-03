d194949feb7614996f57d0ada5be044397f62089
Alexander <nefedov.alexander91@yandex.ru>
2016-03-04 00:53:49 +0500

    [Tutorials] SFML: Added new buttons in MenuState
    Modified:
    	tutorials/airstrike-sfml/Sources/MenuState.cpp

--- tutorials/airstrike-sfml/Sources/MenuState.cpp
+++ tutorials/airstrike-sfml/Sources/MenuState.cpp
@@ -2,6 +2,7 @@
 #include "MenuState.hpp"
 #include "Button.hpp"
 #include "Utility.hpp"
+#include "MusicPlayer.hpp"
 #include "ResourceHolder.hpp"
 
 #include <SFML/Graphics/RenderWindow.hpp>
@@ -11,10 +12,10 @@
 MenuState::MenuState( StateStack& stack, Context context ) :
 State( stack, context ),
 mGUIContainer() {
-    sf::Texture& texture    = context.textures->get( Textures::TitleScreen );
+    sf::Texture& texture = context.textures->get( Textures::TitleScreen );
     mBackgroundSprite.setTexture( texture );
 
-    auto playButton = std::make_shared<GUI::Button>( *context.fonts, *context.textures );
+    auto playButton = std::make_shared<GUI::Button>( context );
     playButton->setPosition( 100, 300 );
     playButton->setText( "Play" );
     playButton->setCallback( [this] () {
@@ -22,23 +23,43 @@ mGUIContainer() {
         requestStackPush( States::Game );
     });
 
-    auto settingsButton = std::make_shared<GUI::Button>( *context.fonts, *context.textures );
-    settingsButton->setPosition( 100, 350 );
+    auto hostButton = std::make_shared<GUI::Button>( context );
+    hostButton->setPosition( 100, 350 );
+    hostButton->setText( "Host" );
+    hostButton->setCallback( [this] () {
+        requestStackPop();
+        requestStackPush( States::HostGame );
+    });
+
+    auto joinButton = std::make_shared<GUI::Button>( context );
+    joinButton->setPosition( 100, 400 );
+    joinButton->setText( "Join" );
+    joinButton->setCallback( [this] () {
+        requestStackPop();
+        requestStackPush( States::JoinGame );
+    });
+
+    auto settingsButton = std::make_shared<GUI::Button>( context );
+    settingsButton->setPosition( 100, 450 );
     settingsButton->setText( "Settings" );
     settingsButton->setCallback( [this] () {
         requestStackPush( States::Settings );
     });
 
-    auto exitButton = std::make_shared<GUI::Button>( *context.fonts, *context.textures );
-    exitButton->setPosition( 100, 400 );
+    auto exitButton = std::make_shared<GUI::Button>( context );
+    exitButton->setPosition( 100, 500 );
     exitButton->setText( "Exit" );
     exitButton->setCallback( [this] () {
         requestStackPop();
     });
 
     mGUIContainer.pack( playButton );
+    mGUIContainer.pack( hostButton );
+    mGUIContainer.pack( joinButton );
     mGUIContainer.pack( settingsButton );
     mGUIContainer.pack( exitButton );
+
+    context.music->play( Music::MenuTheme );
 }
 
 void 
