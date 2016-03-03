6ae35a72b290c6d03b2616b22b524a1ffe0dd9f9
Alexander <nefedov.alexander91@yandex.ru>
2016-03-04 00:30:35 +0500

    [Tutorials] SFML: Data tables remastered
    Modified:
    	tutorials/airstrike-sfml/Sources/DataTables.cpp
    	tutorials/airstrike-sfml/Sources/DataTables.hpp

--- tutorials/airstrike-sfml/Sources/DataTables.cpp
+++ tutorials/airstrike-sfml/Sources/DataTables.cpp
@@ -3,6 +3,7 @@
 #include "Aircraft.hpp"
 #include "Projectile.hpp"
 #include "Pickup.hpp"
+#include "Particle.hpp"
 
 
 using namespace std::placeholders;
@@ -15,25 +16,32 @@ initalizeAircraftData() {
     data[Aircraft::Eagle].hitpoints         = 100;
     data[Aircraft::Eagle].speed             = 200.0f;
     data[Aircraft::Eagle].fireInterval      = sf::seconds( 1 );
-    data[Aircraft::Eagle].texture           = Textures::Eagle;
+    data[Aircraft::Eagle].texture           = Textures::Entities;
+    data[Aircraft::Eagle].textureRect       = sf::IntRect( 0, 0, 48, 64 );
+    data[Aircraft::Eagle].hasRollAnimation  = true;
 
     data[Aircraft::Raptor].hitpoints        = 20;
     data[Aircraft::Raptor].speed            = 80.0f;
-    data[Aircraft::Raptor].texture          = Textures::Raptor;
+    data[Aircraft::Raptor].texture          = Textures::Entities;
+    data[Aircraft::Raptor].textureRect      = sf::IntRect( 144, 0, 84, 64 );
     data[Aircraft::Raptor].directions.push_back( Direction( +45.0f, 80.0f ) );
     data[Aircraft::Raptor].directions.push_back( Direction( -45.0f, 160.0f ) );
     data[Aircraft::Raptor].directions.push_back( Direction( +45.0f, 80.0f ) );
-    data[Aircraft::Raptor].fireInterval     = sf::seconds( 1.8f );
+    data[Aircraft::Raptor].fireInterval     = sf::Time::Zero;
+    data[Aircraft::Raptor].hasRollAnimation = false;
 
-    data[Aircraft::Avenger].hitpoints       = 40;
-    data[Aircraft::Avenger].speed           = 50.0f;
-    data[Aircraft::Avenger].texture         = Textures::Avenger;
+
+    data[Aircraft::Avenger].hitpoints           = 40;
+    data[Aircraft::Avenger].speed               = 50.0f;
+    data[Aircraft::Avenger].texture             = Textures::Entities;
+    data[Aircraft::Avenger].textureRect         = sf::IntRect( 289, 3, 60, 57 );
     data[Aircraft::Avenger].directions.push_back( Direction( +45.0f, 50.0f ) );
     data[Aircraft::Avenger].directions.push_back( Direction( 0.0f, 50.0f ) );
     data[Aircraft::Avenger].directions.push_back( Direction( -45.0f, 100.0f ) );
     data[Aircraft::Avenger].directions.push_back( Direction( 0.0f, 50.0f ) );
     data[Aircraft::Avenger].directions.push_back( Direction( +45.0f, 50.0f ) );
-    data[Aircraft::Avenger].fireInterval    = sf::Time::Zero;
+    data[Aircraft::Avenger].fireInterval        = sf::seconds( 2 );
+    data[Aircraft::Avenger].hasRollAnimation    = false;
 
     return data;
 }
@@ -41,17 +49,21 @@ initalizeAircraftData() {
 std::vector<ProjectileData>
 initalizeProjectileData() {
     std::vector<ProjectileData> data( Projectile::TypeCount );
-    data[Projectile::AlliedBullet].damage   = 10;
-    data[Projectile::AlliedBullet].speed    = 300.0f;
-    data[Projectile::AlliedBullet].texture  = Textures::Bullet;
+    data[Projectile::AlliedBullet].damage       = 10;
+    data[Projectile::AlliedBullet].speed        = 300.0f;
+    data[Projectile::AlliedBullet].texture      = Textures::Entities;
+    data[Projectile::AlliedBullet].textureRect  = sf::IntRect( 175, 64, 3, 14 );
 
-    data[Projectile::EnemyBullet].damage    = 10;
-    data[Projectile::EnemyBullet].speed     = 300.0f;
-    data[Projectile::EnemyBullet].texture   = Textures::Bullet;
+    data[Projectile::EnemyBullet].damage        = 10;
+    data[Projectile::EnemyBullet].speed         = 300.0f;
+    data[Projectile::EnemyBullet].texture       = Textures::Entities;
+    data[Projectile::EnemyBullet].textureRect   = sf::IntRect( 178, 64, 3, 14 );
 
-    data[Projectile::Missile].damage        = 200;
-    data[Projectile::Missile].speed         = 150.0f;
-    data[Projectile::Missile].texture       = Textures::Missile;
+
+    data[Projectile::Missile].damage            = 200;
+    data[Projectile::Missile].speed             = 150.0f;
+    data[Projectile::Missile].texture           = Textures::Entities;
+    data[Projectile::Missile].textureRect       = sf::IntRect( 160, 64, 15, 32 );
 
     return data;
 }
@@ -60,17 +72,34 @@ std::vector<PickupData>
 initalizePickupData() {
     std::vector<PickupData> data( Pickup::TypeCount );
 
-    data[Pickup::HealthRefill].texture  = Textures::HealthRefill;
-    data[Pickup::HealthRefill].action   = [] ( Aircraft& a ) { a.repair( 25 ); };
+    data[Pickup::HealthRefill].texture      = Textures::Entities;
+    data[Pickup::HealthRefill].textureRect  = sf::IntRect( 0, 64, 40, 40 );
+    data[Pickup::HealthRefill].action       = [] ( Aircraft& a ) { a.repair( 25 ); };
 
-    data[Pickup::MissileRefill].texture = Textures::MissileRefill;
-    data[Pickup::MissileRefill].action  = std::bind( &Aircraft::collectMissiles, _1, 3 );
+    data[Pickup::MissileRefill].texture     = Textures::Entities;
+    data[Pickup::MissileRefill].textureRect = sf::IntRect( 40, 64, 40, 40 );
+    data[Pickup::MissileRefill].action      = std::bind( &Aircraft::collectMissiles, _1, 3 );
 
-    data[Pickup::FireSpread].texture    = Textures::FireSpread;
-    data[Pickup::FireSpread].action     = std::bind( &Aircraft::increaseSpread, _1 );
+    data[Pickup::FireSpread].texture        = Textures::Entities;
+    data[Pickup::FireSpread].textureRect    = sf::IntRect( 80, 64, 40, 40 );
+    data[Pickup::FireSpread].action         = std::bind( &Aircraft::increaseSpread, _1 );
 
-    data[Pickup::FireRate].texture      = Textures::FireRate;
-    data[Pickup::FireRate].action       = std::bind( &Aircraft::increaseFireRate, _1 );
+    data[Pickup::FireRate].texture          = Textures::Entities;
+    data[Pickup::FireRate].textureRect      = sf::IntRect( 120, 64, 40, 40 );
+    data[Pickup::FireRate].action           = std::bind( &Aircraft::increaseFireRate, _1 );
+
+    return data;
+}
+
+std::vector<ParticleData>
+initalizeParticleData() {
+    std::vector<ParticleData> data( Particle::ParticleCount );
+
+    data[Particle::Propellant].color    = sf::Color( 255, 255, 50 );
+    data[Particle::Propellant].lifetime = sf::seconds( 0.6f );
+
+    data[Particle::Smoke].color         = sf::Color( 50, 50, 50 );
+    data[Particle::Smoke].lifetime      = sf::seconds( 4.0f );
 
     return data;
 }
--- tutorials/airstrike-sfml/Sources/DataTables.hpp
+++ tutorials/airstrike-sfml/Sources/DataTables.hpp
@@ -9,6 +9,7 @@
 
 #include <SFML/System/Time.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Rect.hpp>
 
 #include <vector>
 #include <functional>
@@ -31,24 +32,34 @@ struct AircraftData {
     int                     hitpoints;
     float                   speed;
     Textures::ID            texture;
+    sf::IntRect             textureRect;
     sf::Time                fireInterval;
     std::vector<Direction>  directions;
+    bool                    hasRollAnimation;
 };
 
 struct ProjectileData {
     int             damage;
     float           speed;
     Textures::ID    texture;
+    sf::IntRect     textureRect;
 };
 
 struct PickupData {
     std::function<void( Aircraft& )>    action;
     Textures::ID                        texture;
+    sf::IntRect                         textureRect;
+};
+
+struct ParticleData {
+    sf::Color color;
+    sf::Time lifetime;
 };
 
 
 std::vector<AircraftData>   initalizeAircraftData();
 std::vector<ProjectileData> initalizeProjectileData();
 std::vector<PickupData>     initalizePickupData();
+std::vector<ParticleData>   initalizeParticleData();
 
-#endif
+#endif // __DATA_TABLES_HPP__
