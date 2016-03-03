d9994c74943604d1a761a47662fba40f68f089d4
Alexander <nefedov.alexander91@yandex.ru>
2016-03-03 23:07:37 +0500

    [Tutorials] New types of categories
    Modified:
    	tutorials/airstrike-sfml/Sources/Category.hpp

--- tutorials/airstrike-sfml/Sources/Category.hpp
+++ tutorials/airstrike-sfml/Sources/Category.hpp
@@ -12,6 +12,9 @@ enum Type {
     Pickup              = 1 << 4,
     AlliedProjectile    = 1 << 5,
     EnemyProjectile     = 1 << 6,
+    ParticleSystem      = 1 << 7,
+    SoundEffect         = 1 << 8,
+    Network             = 1 << 9,
 
     Aircraft            = PlayerAircraft | AlliedAircraft | EnemyAircraft,
     Projectile          = AlliedProjectile | EnemyProjectile,
