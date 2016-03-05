bbed9825205ca47ffe1371ba6bd1712f91133e54
Alexander <nefedov.alexander91@yandex.ru>
2016-03-05 21:45:50 +0500

    [Tutorials] SFML: New MusicPlayer class
    New files:
    	tutorials/airstrike-sfml/Sources/MusicPlayer.cpp
    	tutorials/airstrike-sfml/Sources/MusicPlayer.hpp

---
+++ tutorials/airstrike-sfml/Sources/MusicPlayer.hpp
@@ -0,0 +1,32 @@
+
+#ifndef __MUSIC_PLAYER_HPP__
+#define __MUSIC_PLAYER_HPP__
+
+
+#include "ResourceHolder.hpp"
+#include "ResourceIdentifiers.hpp"
+
+#include <SFML/System/NonCopyable.hpp>
+#include <SFML/Audio/Music.hpp>
+
+#include <map>
+#include <string>
+
+
+class MusicPlayer : private sf::NonCopyable {
+public:
+    MusicPlayer();
+
+    void play( Music::ID theme );
+    void stop();
+
+    void setPaused( bool paused );
+    void setVolume( float volume );
+
+private:
+    sf::Music                           mMusic;
+    std::map<Music::ID, std::string>    mFilenames;
+    float                               mVolume;
+};
+
+#endif // __MUSIC_PLAYER_HPP__
