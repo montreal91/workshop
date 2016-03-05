3490d502994d466c813bafc619ebcda55ddde7b4
Alexander <nefedov.alexander91@yandex.ru>
2016-03-06 01:30:54 +0500

    [Tutorials] SFML: Updated SpriteNode class
    Modified:
    	tutorials/airstrike-sfml/Sources/SpriteNode.cpp
    	tutorials/airstrike-sfml/Sources/SpriteNode.hpp

--- tutorials/airstrike-sfml/Sources/SpriteNode.cpp
+++ tutorials/airstrike-sfml/Sources/SpriteNode.cpp
@@ -5,14 +5,18 @@
 
 
 SpriteNode::SpriteNode( const sf::Texture& texture ) :
-mSprite( texture ) {
-}
+mSprite( texture ) {}
 
-SpriteNode::SpriteNode( const sf::Texture& texture, const sf::IntRect& textureRect ) :
-mSprite( texture, textureRect ) {
-}
+SpriteNode::SpriteNode(
+    const sf::Texture& texture,
+    const sf::IntRect& textureRect
+) :
+mSprite( texture, textureRect ) {}
 
 void
-SpriteNode::drawCurrent( sf::RenderTarget& target, sf::RenderStates states ) const {
+SpriteNode::drawCurrent(
+    sf::RenderTarget& target,
+    sf::RenderStates states
+) const {
     target.draw( mSprite, states );
 }
--- tutorials/airstrike-sfml/Sources/SpriteNode.hpp
+++ tutorials/airstrike-sfml/Sources/SpriteNode.hpp
@@ -10,14 +10,17 @@
 
 class SpriteNode : public SceneNode {
 public:
-    explicit    SpriteNode( const sf::Texture& texture );
-                SpriteNode( const sf::Texture& texture, const sf::IntRect& textureRect );
+    explicit SpriteNode( const sf::Texture& texture );
+    SpriteNode( const sf::Texture& texture, const sf::IntRect& textureRect );
 
 private:
-    virtual void    drawCurrent( sf::RenderTarget& target, sf::RenderStates states ) const;
+    virtual void    drawCurrent(
+        sf::RenderTarget& target,
+        sf::RenderStates states
+    ) const;
 
     sf::Sprite      mSprite;
 
 };
 
-#endif
+#endif // __SPRITE_NODE_HPP__
