4f65ee4390770c44768b2a6a79921f67f891b84b
Alexander <nefedov.alexander91@yandex.ru>
2016-03-09 10:33:59 +0500

    [Tutorials] Changes in first example of learn_c_hard_way
    Deleted:
    	tutorials/learn_c_hard_way/ex1/ex1
    
    Modified:
    	tutorials/learn_c_hard_way/ex1/ex1.c

--- tutorials/learn_c_hard_way/ex1/ex1.c
+++ tutorials/learn_c_hard_way/ex1/ex1.c
@@ -1,10 +1,41 @@
 
 #include <stdio.h>
 
+int get_days_in_month( int month, int year ) {
+    if ( month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 ) {
+        return 31;
+    } else if ( month == 4 || month == 6 || month == 9 || month == 11 ) {
+        return 30;
+    } else if ( month == 2 && year % 4 == 0 ) {
+        return 29;
+    } else if ( month == 2 && year % 4 != 0 ) {
+        return 28;
+    } else {
+        return -1;
+    }
+}
+
 int main( int argc, char* argv[] ) {
-    puts( "Hello, World!" );
-    puts( "And fuck everything!" );
-    puts( "I've got tired from this shit." );
-    
+    int year, month, day;
+    int day_of_week = 4;
+    for ( year=1994; year<=1995; year++ ) {
+        for ( month=1; month<=12; month++ ) {
+            for ( day=1; day<=get_days_in_month( month, year ); day++ ) {
+                if ( day == 1 ) {
+                    int tab;
+                    for ( tab=1; tab<day_of_week-1; tab++ ) {
+                        printf( "   " );
+                    }
+                }
+                printf("%.2i ", day );
+                if ( day_of_week > 7 ) {
+                    day_of_week = 1;
+                    printf("\n");
+                }
+                day_of_week++;
+            }
+            printf("\n\n");
+        }
+    }
     return 0;
 }
