9937a35f1bda4e98790aad8cac28fdb46f18354b
Alexander <nefedov.alexander91@yandex.ru>
2016-04-09 16:41:53 +0500

    [Jumble] Compiling C++ code with Clang
    New files:
    	jumble/clang-hello/SConstruct
    	jumble/clang-hello/main.cc

---
+++ jumble/clang-hello/main.cc
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include <cmath>
+
+int main( int argc, char const *argv[ ]) {
+    std::cout << "Hello, Clang!\n";
+    std::cout << std::sqrt( 3.0f ) << "\n";
+    return 0;
+}
