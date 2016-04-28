65b652ebde72694c50f1b9cedf97c8c36deccac2
Alexander <nefedov.alexander91@yandex.ru>
2016-04-28 14:34:37 +0500

    [Jumble] BoxAndHits: class for interactive key binding
    Should load key bindings from file, modify it if necessary and then write
    them to file again.
    
    Part of input handler system
    
    New files:
    	jumble/box_and_hits/key_binding.cc
    	jumble/box_and_hits/key_binding.h

---
+++ jumble/box_and_hits/key_binding.h
@@ -0,0 +1,45 @@
+
+#pragma once
+
+
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <SFML/Window/Keyboard.hpp>
+
+
+namespace ih {
+namespace player_action {
+enum EeType {
+    MoveForward,
+    MoveBackward,
+    RotateClockwise,
+    RotateCounterClockwise,
+    ActivateWeapon,
+    DropBomb,
+    Count,
+};
+} // namespace player_action
+
+class EKeyBinding {
+public:
+    typedef player_action::EeType Action_t;
+
+    explicit EKeyBinding();
+
+    void                    AssignKey( Action_t action, sf::Keyboard::Key key );
+    sf::Keyboard::Key       GetAssignedKey( Action_t action ) const;
+
+    bool                    CheckAction( sf::Keyboard::Key key, Action_t& out ) const;
+    std::vector<Action_t>   GetActiveRealtimeActions() const;
+
+private:
+    void        InitalizeActions();
+    static bool IsRealtimeAction( player_action::EeType action );
+
+    std::map<sf::Keyboard::Key, Action_t> m_key_map;
+};
+
+} // namespace ih
