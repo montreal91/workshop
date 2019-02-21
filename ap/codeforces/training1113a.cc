415198a651907441c6381e15e6b9b64d74ad55ee
Alexander Nefedov <montreal91@users.noreply.github.com>
2019-02-21 15:10:02 +0700

    [AP] Codeforces: 1113A - Sasha and His Trip

---
+++ ap/codeforces/training1113a.cc
@@ -0,0 +1,88 @@
+
+//
+// Problem: https://codeforces.com/contest/1113/problem/A
+// Author: montreal91
+// Type: training
+// Time: inf
+// Failed attempts: 1
+//
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+void debug_print(T val, const string& label="") {
+  #ifndef ONLINE_JUDGE
+    string separator;
+    if (label.size() == 0) {
+      separator = "";
+    }
+    else {
+      separator = " ";
+    }
+    cout << label << separator << val << endl;
+  #endif // ONLINE_JUDGE
+}
+
+template<typename T>
+void debug_print(
+  const vector<T>& to_print,
+  const string& separator=" ",
+  const string& label=""
+) {
+  #ifndef ONLINE_JUDGE
+    for (T p : to_print) {
+      cout << p << separator;
+    }
+    cout << endl;
+  #endif // ONLINE_JUDGE
+}
+
+int calc_cost(int cities, int tank) {
+  if (cities - tank <= 1) {
+    return min(cities - 1, tank);
+  }
+  int k = cities - tank - 1;
+  return tank + (k * (k + 3) / 2);
+}
+
+void function(istream& in, ostream& out) {
+  ios::sync_with_stdio(false);
+  in.tie(nullptr);
+  int n, t;
+  in >> n >> t;
+  out << calc_cost(n , t) << endl;
+}
+
+int main() {
+  #ifndef ONLINE_JUDGE
+    using namespace chrono;
+    auto time1 = duration_cast<milliseconds>(
+      system_clock::now().time_since_epoch()
+    );
+  #endif // ONLINE_JUDGE
+
+  function(cin, cout);
+
+  #ifndef ONLINE_JUDGE
+    auto time2 = duration_cast<milliseconds>(
+      system_clock::now().time_since_epoch()
+    );
+    cout << "Time consumed: " << milliseconds(time2 - time1).count();
+    cout << " ms.\n";
+  #endif // ONLINE_JUDGE
+
+  return 0;
+}
