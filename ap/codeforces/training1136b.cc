e4066aa51b41210b1f012d494b62d1e08137cac8
Alexander Nefedov <montreal91@users.noreply.github.com>
2019-03-21 13:05:40 +0700

    [AP] Codeforces 1136B - Nastya Is Playing Computer Games

---
+++ ap/codeforces/training1136b.cc
@@ -0,0 +1,81 @@
+
+//
+// Problem: https://codeforces.com/contest/1136/problem/B
+// Author: montreal91
+// Type: training
+// Time: 00:30
+// Failed attempts: 0
+//
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+void debug_print(T val, const string& label="") {
+  #ifndef ONLINE_JUDGE
+    string separator;
+    if (label.size() == 0) {
+      separator = "";
+    }
+    else {
+      separator = " ";
+    }
+    cout << label << separator << val << endl;
+  #endif // ONLINE_JUDGE
+}
+
+template<typename T>
+void debug_print(
+  const vector<T>& to_print,
+  const string& separator=" ",
+  const string& label=""
+) {
+  #ifndef ONLINE_JUDGE
+    for (T p : to_print) {
+      cout << p << separator;
+    }
+    cout << endl;
+  #endif // ONLINE_JUDGE
+}
+
+void function(istream& in, ostream& out) {
+  ios::sync_with_stdio(false);
+  in.tie(nullptr);
+  int n, k;
+  in >> n >> k;
+
+  out << n * 3 + min(k - 1, n - k) << endl;
+}
+
+int main() {
+  #ifndef ONLINE_JUDGE
+    using namespace chrono;
+    auto time1 = duration_cast<milliseconds>(
+      system_clock::now().time_since_epoch()
+    );
+  #endif // ONLINE_JUDGE
+
+  function(cin, cout);
+
+  #ifndef ONLINE_JUDGE
+    auto time2 = duration_cast<milliseconds>(
+      system_clock::now().time_since_epoch()
+    );
+    cout << "Time consumed: " << milliseconds(time2 - time1).count();
+    cout << " ms.\n";
+  #endif // ONLINE_JUDGE
+
+  return 0;
+}
