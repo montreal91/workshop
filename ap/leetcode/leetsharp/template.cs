7954fca468b5acd1c3ab35c50184beb196a5f439
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2020-11-09 19:10:45 +0700

    [AP] LeetCode - C# template

---
+++ ap/leetcode/leetsharp/template.cs
@@ -0,0 +1,17 @@
+
+using System;
+using System.Diagnostics;
+
+
+namespace template {
+
+
+class Solution {
+  public static void TestSolution() {
+    Debug.Assert(true);
+    Console.WriteLine("All tests passed.");
+  }
+}
+
+
+} // namespace template
