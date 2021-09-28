5296f8dba8a9c522f18d674b8c027302358ca89a
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2021-09-28 20:17:42 +0700

    [AP] CF: java template

---
+++ ap/codeforces/_java_template/SolutionC.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.util.*;
+
+
+
+public class SolutionC {
+    public static void main(String[] args) throws Exception {
+        function(new FastScanner(), System.out);
+    }
+
+    private static void function(final FastScanner in, final PrintStream out) throws Exception {
+    }
+}
+
+class FastScanner {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer("");
+
+    String next() {
+        while (!st.hasMoreTokens()) {
+            try {
+                st=new StringTokenizer(br.readLine());
+            } catch (IOException e) {}
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+}
