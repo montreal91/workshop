c6c15819b5fc32fea321193673dbb1aff368b2fa
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2022-11-19 02:30:04 +0500

    [Jumble] Migrate to Bevy 0.9

--- jumble/rust/my_bevy_game/src/main.rs
+++ jumble/rust/my_bevy_game/src/main.rs
@@ -1,53 +1,8 @@
-use bevy::prelude::*;
 
+use bevy::prelude::App;
+use bevy::prelude::DefaultPlugins;
 
-#[derive(Component)]
-struct Person;
-
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Default)]
-struct GreetTimer(Timer);
-
-
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert_bundle((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn().insert_bundle((Person, Name("Renzo Hume".to_string())));
-    commands.spawn().insert_bundle((Person, Name("Zayna Nieves".to_string())));
-}
-
-
-fn print_greetings(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
-    }
-    println!();
-}
-
-
-fn greet_people(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    query: Query<&Name, With<Person>>
-) {
-    if timer.0.tick(time.delta()).just_finished() {
-        print_greetings(query);
-    }
-}
-
-
-pub struct HelloPlugin;
-
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
-}
+use my_bevy_game::HelloPlugin;
 
 
 fn main() {
